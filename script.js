300ea9643fb74c5504c6d9fdace17c1348f39a5a
log210788 <lewisoshea21@gmail.com>
2026-09-17 17:30:49 +0700

    Initial commit for No More Drama Please site

---
+++ script.js
@@ -0,0 +1,7 @@
+// ==========================================================================
+// Main Application JavaScript
+// ==========================================================================
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM fully loaded and parsed. Ready to write your JS code!');
+});
